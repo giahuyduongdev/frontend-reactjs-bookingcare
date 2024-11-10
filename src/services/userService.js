578afee7dfa5660620360f50ad2a9cce24441c53
giahuyduongdev <giahuyduongdev@gmail.com>
2024-11-10 23:34:01 +0700

    Modal Edit User, call API NodeJS editUserService

--- src/services/userService.js
+++ src/services/userService.js
@@ -20,11 +20,15 @@ const deleteUserService = (userId) => {
   });
 };
 
+const editUserService = (inputData) => {
+  return axios.put("/api/edit-user", inputData);
+};
 
 
 export {
   handleLoginApi,
   getAllUsers,
   createNewUserService,
-  deleteUserService
+  deleteUserService,
+  editUserService
 }

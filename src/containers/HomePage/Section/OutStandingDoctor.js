512f3e3c4c5ba1be1a692f52be9eb1c29166324e
giahuyduongdev <giahuyduongdev@gmail.com>
2024-11-14 04:12:40 +0700

    Auto load top doctor at homepage

--- src/containers/HomePage/Section/OutStandingDoctor.js
+++ src/containers/HomePage/Section/OutStandingDoctor.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
 import { FormattedMessage } from "react-intl";
 import Slider from "react-slick";
-
-
-
+import * as actions from "../../../store/actions";
+import { LANGUAGES } from "../../../utils";
+import { withRouter } from "react-router";
 class OutStandingDoctor extends Component {
   constructor(props) {
     super(props);
@@ -12,107 +13,94 @@ class OutStandingDoctor extends Component {
       arrDoctors: [],
     };
   }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.topDoctorsRedux !== this.props.topDoctorsRedux) {
+      this.setState({
+        arrDoctors: this.props.topDoctorsRedux,
+      });
+    }
+  }
 
-    render() {
-      return(
-        <div className="section-share section-outstanding-doctor">
+  componentDidMount() {
+    this.props.loadTopDoctors();
+  }
+
+  handleViewDetailDoctor = (doctor) => {
+    if (this.props.history) {
+      this.props.history.push(`/detail-doctor/${doctor.id}`);
+    }
+  };
+  handleOnClickSeeMoreDoctor = () => {
+    if (this.props.history) {
+      this.props.history.push(`/list-oustanding-doctor`);
+    }
+  };
+  render() {
+    let arrDoctors = this.state.arrDoctors;
+    let { language } = this.props;
+    // arrDoctors = arrDoctors
+    //   .concat(arrDoctors)
+    //   .concat(arrDoctors)
+    //   .concat(arrDoctors)
+    //   .concat(arrDoctors);
+    return (
+      <div className="section-share section-outstanding-doctor">
         <div className="section-container">
           <div className="section-header">
-            <span className="title-section">Bác sĩ nổi bật tuần qua </span>
-            <button className="btn-section">Tìm kiếm  </button>
+            <span className="title-section">
+              <FormattedMessage id="homepage.outstanding-doctor" />
+            </span>
+            <button
+              className="btn-section"
+              onClick={() => this.handleOnClickSeeMoreDoctor()}
+            >
+              <FormattedMessage id="homepage.more-infor" />
+            </button>
           </div>
           <div className="section-body">
-          <Slider {...this.props.settings}>
-              <div className="section-customize">
-                <div className="customize-border">
-                  <div className="outer-bg">
-                    <div className="bg-image section-outstanding-doctor"></div>
-                    </div>
-                    <div className="position text-center">
-                      <div>
-                        Bác sĩ 1
-                      </div>
-                      <div>
-                        Chuyên khoa 1
-                      </div>
-                    </div>
-                </div>
-              </div>
-              <div className="section-customize">
-                <div className="customize-border">
-                  <div className="outer-bg">
-                    <div className="bg-image section-outstanding-doctor"></div>
-                    </div>
-                    <div className="position text-center">
-                      <div>
-                        Bác sĩ 2
-                      </div>
-                      <div>
-                        Chuyên khoa 2
-                      </div>
-                    </div>
-                </div>
-              </div>
-              <div className="section-customize">
-                <div className="customize-border">
-                  <div className="outer-bg">
-                    <div className="bg-image section-outstanding-doctor"></div>
-                    </div>
-                    <div className="position text-center">
-                      <div>
-                        Bác sĩ 3
-                      </div>
-                      <div>
-                        Chuyên khoa 3
+            <Slider {...this.props.settings}>
+              {arrDoctors &&
+                arrDoctors.length > 0 &&
+                arrDoctors.map((item, index) => {
+                  let imageBase64 = "";
+                  if (item.image) {
+                    imageBase64 = new Buffer(item.image, "base64").toString(
+                      "binary"
+                    );  
+                  }
+                  let nameVi = `${item.positionData.valueVi}, ${item.lastName} ${item.firstName}`;
+                  let nameEn = `${item.positionData.valueEn}, ${item.firstName} ${item.lastName}`;
+                  return (
+                    <div
+                      className="section-customize"
+                      key={index}
+                      onClick={() => this.handleViewDetailDoctor(item)}
+                    >
+                      <div className="customize-border">
+                        <div className="outer-bg">
+                          <div
+                            className="bg-image section-outstanding-doctor"
+                            style={{
+                              backgroundImage: `url(${imageBase64})`,
+                            }}
+                          ></div>
+                        </div>
+                        <div className="position text-center">
+                          <div>
+                            {language === LANGUAGES.VI ? nameVi : nameEn}
+                          </div>
+                          <div>
+                            {item.Doctor_Infor &&
+                            item.Doctor_Infor.specialtyData &&
+                            item.Doctor_Infor.specialtyData.name
+                              ? item.Doctor_Infor.specialtyData.name
+                              : ""}
+                          </div>
+                        </div>
                       </div>
                     </div>
-                </div>
-              </div>
-              <div className="section-customize">
-                <div className="customize-border">
-                  <div className="outer-bg">
-                    <div className="bg-image section-outstanding-doctor"></div>
-                    </div>
-                    <div className="position text-center">
-                      <div>
-                        Bác sĩ 4
-                      </div>
-                      <div>
-                        Chuyên khoa 4
-                      </div>
-                    </div>
-                </div>
-              </div>
-              <div className="section-customize">
-                <div className="customize-border">
-                  <div className="outer-bg">
-                    <div className="bg-image section-outstanding-doctor"></div>
-                    </div>
-                    <div className="position text-center">
-                      <div>
-                        Bác sĩ 5
-                      </div>
-                      <div>
-                        Chuyên khoa 5
-                      </div>
-                    </div>
-                </div>
-              </div>
-              <div className="section-customize">
-                <div className="customize-border">
-                  <div className="outer-bg">
-                    <div className="bg-image section-outstanding-doctor"></div>
-                    </div>
-                    <div className="position text-center">
-                      <div>
-                        Bác sĩ 6
-                      </div>
-                      <div>
-                        Chuyên khoa 6
-                      </div>
-                    </div>
-                </div>
-              </div>
+                  );
+                })}
             </Slider>
           </div>
         </div>
@@ -121,15 +109,20 @@ class OutStandingDoctor extends Component {
   }
 }
 
-const mapStateToProps = state => {
-    return {
-        isLoggedIn: state.user.isLoggedIn
-    };
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    topDoctorsRedux: state.admin.topDoctors,
+    language: state.app.language,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadTopDoctors: () => dispatch(actions.fetchTopDoctor()),
+  };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(OutStandingDoctor);
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(OutStandingDoctor)
+);
